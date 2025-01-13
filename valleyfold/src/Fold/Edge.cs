fa6887c3e681f1bab43292febe9e06de0abfac53
Emil Bohleber <me@limered.de>
2025-01-13 22:49:59 +0100

    add comments to edge model

--- valleyfold/src/Fold/Edge.cs
+++ valleyfold/src/Fold/Edge.cs
@@ -4,8 +4,15 @@ namespace valleyfold.Fold;
 
 public class Edge
 {
-    public Id[] Vertices = new Id[2]; 
-    public List<Id> Faces;
+    /**
+     * Vertices[0] is first
+     */
+    public Id[] Vertices = new Id[2];
+    
+    /**
+     * Faces[0] is the LEFT face
+     */
+    public Id[] Faces = new Id[2];
     public Assignment Assignment;
     public float FoldAngle;
     public float Length;
