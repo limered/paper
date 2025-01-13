ba9e86108c2a6c00611146f6b1e53446a3ac09e3
Emil Bohleber <me@limered.de>
2025-01-13 11:44:52 +0100

    remove old interaction system

--- valleyfold/src/Game.cs
+++ valleyfold/src/Game.cs
@@ -1,27 +1,14 @@
 using Godot;
-using valleyfold.v1.Interaction;
-using valleyfold.v1.Models;
 
 namespace valleyfold;
 
 public partial class Game : Node
 {
-    private PaperModel _paperModel;
-    private InteractionSystem _interactionSystem;
-
     public override void _Ready()
     {
-        _paperModel = new PaperModel();
-        _paperModel.AddFace(Statics.StartingPaper);
-
-        _interactionSystem = new InteractionSystem(_paperModel.Faces)
-        {
-            ModelParent = this
-        };
     }
 
     public override void _Process(double delta)
     {
-        _interactionSystem.Process();
     }
 }
