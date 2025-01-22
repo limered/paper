45911e6c6f528f4812669f4a18d2bee8e7f39eb9
Emil Bohleber <me@limered.de>
2025-01-22 14:12:28 +0100

    add valleyfolde class

---
+++ valleyfold/src/Folding/IFold.cs
@@ -0,0 +1,8 @@
+using valleyfold.Fold;
+
+namespace valleyfold.Folding;
+
+public interface IFold
+{
+    void Apply(Frame frame);
+}
---
+++ valleyfold/src/Folding/Valleyfold.cs
@@ -0,0 +1,13 @@
+using valleyfold.Fold;
+
+namespace valleyfold.Folding;
+
+public class Valleyfold : IFold
+{
+    public Id Vertices;
+    
+    public void Apply(Frame frame)
+    {
+        throw new System.NotImplementedException();
+    }
+}
