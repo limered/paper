92511be4b8ad37de5d5c63459d87023bd50e052a
Emil Bohleber <me@limered.de>
2025-01-25 15:21:38 +0100

    ui - visualize new vertices

--- valleyfold/src/Interaction/PaperInteractions/ClickSystem.cs
+++ valleyfold/src/Interaction/PaperInteractions/ClickSystem.cs
@@ -14,10 +14,14 @@ public class WorldNewPointData
 public partial class ClickSystem : Node3D
 {
     [Export] public Node3D GhostClickPosition;
+    [Export] public Node3D FirstNewVertex;
+    [Export] public Node3D SecondNewVertex;
     [Export] public Area3D MouseCollision;
 
     private Vector3 _mouseWorldPosition;
-    private WorldNewPointData _newPointData;
+    private readonly WorldNewPointData _tempPointData = new();
+    private WorldNewPointData _firstPoint;
+    private WorldNewPointData _secondPoint;
 
     public override void _Ready()
     {
@@ -36,11 +40,35 @@ public partial class ClickSystem : Node3D
 
     public override void _Input(InputEvent @event)
     {
-        if (@event is InputEventMouseButton mouseEvent)
-            if (mouseEvent.ButtonIndex == MouseButton.Left && mouseEvent.Pressed)
+        if (@event is not InputEventMouseButton mouseEvent) return;
+        if (mouseEvent.ButtonIndex == MouseButton.Left && mouseEvent.Pressed)
+        {
+            if (_firstPoint == null)
             {
-                // accept point
+                _firstPoint = new WorldNewPointData()
+                {
+                    Coord = _tempPointData.Coord,
+                    ExistingVertex = _tempPointData.ExistingVertex,
+                    IsOnEdge = _tempPointData.IsOnEdge
+                };
             }
+            else if (_secondPoint == null)
+            {
+                _secondPoint = new WorldNewPointData()
+                {
+                    Coord = _tempPointData.Coord,
+                    ExistingVertex = _tempPointData.ExistingVertex,
+                    IsOnEdge = _tempPointData.IsOnEdge
+                };
+                
+                // accept cut
+            }
+            else
+            {
+                _firstPoint = null;
+                _secondPoint = null;
+            }
+        }
     }
 
     public override void _Process(double delta)
@@ -48,20 +76,23 @@ public partial class ClickSystem : Node3D
         if (Statics.Frame == null) return;
 
         var nearestEdges = Statics.Frame.NearestEdgesTo(_mouseWorldPosition);
-        _newPointData.ExistingVertex = Statics.Frame.NearestVertexIdTo(_mouseWorldPosition);
-        _newPointData.Coord = Statics.Frame.Vertices[_newPointData.ExistingVertex].Coord;
-        _newPointData.IsOnEdge = false;
+        _tempPointData.ExistingVertex = Statics.Frame.NearestVertexIdTo(_mouseWorldPosition);
+        _tempPointData.Coord = Statics.Frame.Vertices[_tempPointData.ExistingVertex].Coord;
+        _tempPointData.IsOnEdge = false;
         if (nearestEdges.Any())
         {
             var pointOnEdge = Statics.Frame.NearestPointOnEdgeTo(_mouseWorldPosition, nearestEdges);
             if (pointOnEdge.DistanceSquaredTo(_mouseWorldPosition) <
-                _newPointData.Coord.DistanceSquaredTo(_mouseWorldPosition))
+                _tempPointData.Coord.DistanceSquaredTo(_mouseWorldPosition))
             {
-                _newPointData.Coord = pointOnEdge;
-                _newPointData.IsOnEdge = true;
+                _tempPointData.Coord = pointOnEdge;
+                _tempPointData.IsOnEdge = true;
             }
         }
 
-        GhostClickPosition.Position = _newPointData.Coord;
+        GhostClickPosition.Position = _tempPointData.Coord;
+
+        FirstNewVertex.Position = _firstPoint?.Coord ?? new Vector3(-100, 0, -100);
+        SecondNewVertex.Position = _secondPoint?.Coord ?? new Vector3(-100, 0, -100);
     }
 }
