bfaaa17a1c7c8fe1a009431145a6498add2524e9
Emil Bohleber <me@limered.de>
2025-01-28 22:35:41 +0100

    ui - render vertices as red circles

--- valleyfold/src/Render/Vertices/VertexRendering.cs
+++ valleyfold/src/Render/Vertices/VertexRendering.cs
@@ -5,11 +5,16 @@ namespace valleyfold.Render.Vertices;
 public partial class VertexRendering : MeshInstance3D
 {
     private ArrayMesh _mesh;
+    private ShaderMaterial _shader;
     
     public override void _Ready()
     {
         _mesh = new ArrayMesh();
         Mesh = _mesh;
+
+        _shader = new ShaderMaterial();
+        _shader.Shader = ResourceLoader.Load<Shader>("res://src/Render/Vertices/vertex.gdshader");
+        MaterialOverride = _shader;
     }
 
     public override void _Process(double delta)
@@ -20,6 +25,7 @@ public partial class VertexRendering : MeshInstance3D
         _mesh.ClearSurfaces();
         var st = new SurfaceTool();
         st.Begin(Mesh.PrimitiveType.Triangles);
+        st.SetCustomFormat(0, SurfaceTool.CustomFormat.Max); // ToDo add custom attributes for size
         
         for (var i = 0; i < frame.Vertices.Count; i++)
         {
@@ -31,21 +37,20 @@ public partial class VertexRendering : MeshInstance3D
 
     private static void AddQuad(SurfaceTool st, Vector3 offset, int index)
     {
-        st.SetUV(new Vector2(0, 0));
-        st.AddVertex(new Vector3(0, 0, 0) + offset);
+        const float size = 0.02f;
         
+        st.SetUV(new Vector2(0, 0));
+        st.AddVertex(new Vector3(-size, 0, -size) + offset);
         
         st.SetUV(new Vector2(1, 0));
-        st.AddVertex(new Vector3(1, 0, 0) + offset);
-        
+        st.AddVertex(new Vector3(size, 0, -size) + offset);
         
         st.SetUV(new Vector2(1, 1));
-        st.AddVertex(new Vector3(1, 1, 0) + offset);
-        
+        st.AddVertex(new Vector3(size, 0, size) + offset);
         
         st.SetUV(new Vector2(0, 1));
-        st.AddVertex(new Vector3(0, 1, 0) + offset);
-
+        
+        st.AddVertex(new Vector3(-size, 0, size) + offset);
 
         // Add custom attribute for circle properties
         // st.AddCustom(new Vector2((float)index / quadCount, GD.Randf()));
