aec1c2279cdb40d6fc3bb04d16e4dc9fa674e0da
Emil Bohleber <me@limered.de>
2025-01-28 18:27:23 +0100

    ui - use array mesh instead of multimesh

--- valleyfold/src/Render/Vertices/VertexRendering.cs
+++ valleyfold/src/Render/Vertices/VertexRendering.cs
@@ -2,31 +2,59 @@ using Godot;
 
 namespace valleyfold.Render.Vertices;
 
-public partial class VertexRendering : MultiMeshInstance3D
+public partial class VertexRendering : MeshInstance3D
 {
-    private Mesh _baseMesh;
+    private ArrayMesh _mesh;
     
     public override void _Ready()
     {
-        _baseMesh = new BoxMesh();
-        ((BoxMesh)_baseMesh).Size = new Vector3(0.5f, 0.5f, 0.5f);
-        Multimesh.TransformFormat = MultiMesh.TransformFormatEnum.Transform3D;
-        Multimesh.InstanceCount = 0;
-        MaterialOverride = new StandardMaterial3D();
+        _mesh = new ArrayMesh();
+        Mesh = _mesh;
     }
 
     public override void _Process(double delta)
     {
         if (Statics.Frame == null) return;
         var frame = Statics.Frame;
-        
-        Multimesh.InstanceCount = frame.Vertices.Count;
-        Multimesh.Mesh = _baseMesh;
 
+        _mesh.ClearSurfaces();
+        var st = new SurfaceTool();
+        st.Begin(Mesh.PrimitiveType.Triangles);
+        
         for (var i = 0; i < frame.Vertices.Count; i++)
         {
-            var trans = new Transform3D().Translated(frame.Vertices[i].Coord);
-            Multimesh.SetInstanceTransform(i, trans);
+            AddQuad(st, frame.Vertices[i].Coord, i);
         }
+
+        st.Commit(_mesh);
+    }
+
+    private static void AddQuad(SurfaceTool st, Vector3 offset, int index)
+    {
+        st.SetUV(new Vector2(0, 0));
+        st.AddVertex(new Vector3(0, 0, 0) + offset);
+        
+        
+        st.SetUV(new Vector2(1, 0));
+        st.AddVertex(new Vector3(1, 0, 0) + offset);
+        
+        
+        st.SetUV(new Vector2(1, 1));
+        st.AddVertex(new Vector3(1, 1, 0) + offset);
+        
+        
+        st.SetUV(new Vector2(0, 1));
+        st.AddVertex(new Vector3(0, 1, 0) + offset);
+
+
+        // Add custom attribute for circle properties
+        // st.AddCustom(new Vector2((float)index / quadCount, GD.Randf()));
+
+        st.AddIndex(index * 4 + 0);
+        st.AddIndex(index * 4 + 1);
+        st.AddIndex(index * 4 + 2);
+        st.AddIndex(index * 4 + 0);
+        st.AddIndex(index * 4 + 2);
+        st.AddIndex(index * 4 + 3);
     }
 }
