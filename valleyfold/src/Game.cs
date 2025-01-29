6b8a961eaef087ca8b7b364597ccefd6aa2d115e
Emil Bohleber <me@limered.de>
2025-01-29 14:00:24 +0100

    refactor - remove model renderer and move initial paper to game node

--- valleyfold/src/Game.cs
+++ valleyfold/src/Game.cs
@@ -1,4 +1,5 @@
 using Godot;
+using valleyfold.Fold;
 
 namespace valleyfold;
 
@@ -6,6 +7,8 @@ public partial class Game : Node
 {
     public override void _Ready()
     {
+        Statics.Frame = new Frame();
+        Statics.Frame.InitializePaper();
     }
 
     public override void _Process(double delta)
