ad118f9d7d722d2ff3f529ba1d6e2a781d247c66
Emil Bohleber <me@limered.de>
2025-02-03 17:50:32 +0100

    algo - calculate edge crossings

--- valleyfold/src/FrameModifications/EdgeQueries.cs
+++ valleyfold/src/FrameModifications/EdgeQueries.cs
@@ -53,4 +53,42 @@ public static class EdgeQueries
         var startToEnd = end - start;
         return startToPoint.Project(startToEnd) + start;
     }
+
+    public static List<Edge> EdgesCrossingEdge(Frame frame, Edge edge)
+    {
+        var crossedEdges = new List<Edge>();
+        foreach (var frameEdge in frame.Edges)
+        {
+            if (frameEdge == edge) continue;
+            if (edge.Vertices[0] == frameEdge.Vertices[0] || edge.Vertices[0] == frameEdge.Vertices[1] ||
+                edge.Vertices[1] == frameEdge.Vertices[0] || edge.Vertices[1] == frameEdge.Vertices[1]) continue;
+
+            if (EdgesIntersect(
+                    frame.Vertices[edge.Vertices[0]].Coord,
+                    frame.Vertices[edge.Vertices[1]].Coord,
+                    frame.Vertices[frameEdge.Vertices[0]].Coord,
+                    frame.Vertices[frameEdge.Vertices[1]].Coord))
+                crossedEdges.Add(frameEdge);
+        }
+
+        return crossedEdges;
+    }
+
+    private static bool EdgesIntersect(Vector3 start1, Vector3 end1, Vector3 start2, Vector3 end2)
+    {
+        float x1 = start1.X, y1 = start1.Z;
+        float x2 = end1.X, y2 = end1.Z;
+        float x3 = start2.X, y3 = start2.Z;
+        float x4 = end2.X, y4 = end2.Z;
+
+        var denominator = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+
+        if (denominator == 0)
+            return false; // Lines are parallel
+
+        var ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denominator;
+        var ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denominator;
+
+        return ua is >= 0 and <= 1 && ub is >= 0 and <= 1;
+    }
 }
