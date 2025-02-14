ba8f88fad48100c7db6b5a6cdd48b3d9be46c6f6
Emil Bohleber <me@limered.de>
2025-02-15 00:08:03 +0100

    refactor - move cross edge code to new classes

--- Testing/Folding/VertexToVertexFoldTests.cs
+++ Testing/Folding/VertexToVertexFoldTests.cs
@@ -1,3 +1,4 @@
+using Godot;
 using valleyfold.Fold;
 using valleyfold.Folding;
 
@@ -19,33 +20,87 @@ public class VertexToVertexFoldTests
         public void IfVerticesAreTheSame_DoNotChangeFrame()
         {
             new VertexToVertexFold(0, 0).Apply(_testingFrame);
-            
+
             Assert.Equivalent(1, _testingFrame.Faces.Count);
         }
-        
+
         [Fact]
         public void IfVerticesLieTheSameEdge_DoNotChangeFrame()
         {
             new VertexToVertexFold(0, 1).Apply(_testingFrame);
-            
+
             Assert.Equivalent(1, _testingFrame.Faces.Count);
         }
-        
+
         [Fact]
         public void CreateANewEdgeBetweenPoints()
         {
             new VertexToVertexFold(0, 2).Apply(_testingFrame);
-            
+
             Assert.Equivalent(new Id[] { 0, 2 }, _testingFrame.Edges.Last().Vertices);
         }
-        
+
         [Fact]
         public void CreateTwoNewFaces()
         {
             new VertexToVertexFold(0, 2).Apply(_testingFrame);
-            
+
             Assert.Equivalent(new Id[] { 0, 1, 2 }, _testingFrame.Faces.ElementAt(1).Vertices);
             Assert.Equivalent(new Id[] { 2, 3, 0 }, _testingFrame.Faces.ElementAt(0).Vertices);
+        }
+    }
+
+    public class CrossingFolds : VertexToVertexFoldTests
+    {
+        [Fact]
+        public void CrossFoldEdges()
+        {
+            SplitHorizontally();
+            SplitVertically();
+
+            Assert.Equivalent(4, _testingFrame.Faces.Count);
+            Assert.Equivalent(new Vertex { Coord = new Vector3(0.5f, 0, 0.5f) }, _testingFrame.Vertices[8]);
+        }
+
+        [Fact]
+        public void CrossFoldVertices()
+        {
+            new VertexToVertexFold(0, 2).Apply(_testingFrame);
+            new VertexToVertexFold(1, 3).Apply(_testingFrame);
+
+            Assert.Equivalent(4, _testingFrame.Faces.Count);
+            Assert.Equivalent(new Vertex { Coord = new Vector3(0.5f, 0, 0.5f) }, _testingFrame.Vertices[4]);
+        }
+
+        [Fact]
+        public void CrossFoldThenEdgeFold()
+        {
+            SplitHorizontally();
+            SplitVertically();
+            new VertexToVertexFold(0, 2).Apply(_testingFrame);
+
+            Assert.Equivalent(6, _testingFrame.Faces.Count);
+            Assert.Equal(1, _testingFrame.Vertices.Count(v => v == new Vertex { Coord = new Vector3(0.5f, 0, 0.5f) }));
+        }
+
+        private void SplitHorizontally()
+        {
+            var pointA = new Vector3(0, 0, 0.5f);
+            var pointB = new Vector3(1f, 0, 0.5f);
+            var edgeA = _testingFrame.Edges.ElementAt(3);
+            var edgeB = _testingFrame.Edges.ElementAt(1);
+
+            new EdgeToEdgeFold(edgeA, edgeB, pointA, pointB).Apply(_testingFrame);
+        }
+
+        private void SplitVertically()
+        {
+            var pointA = new Vector3(0.5f, 0, 0);
+            var pointB = new Vector3(0.5f, 0, 1f);
+            var edgeA = _testingFrame.Edges.ElementAt(0);
+            var edgeB = _testingFrame.Edges.ElementAt(2);
+
+            new EdgeToEdgeFold(edgeA, edgeB, pointA, pointB).Apply(_testingFrame);
         }
     }
 }
--- valleyfold/src/Folding/VertexToVertexFold.cs
+++ valleyfold/src/Folding/VertexToVertexFold.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Linq;
+using Godot;
 using valleyfold.Fold;
 using valleyfold.FrameModifications;
 
@@ -17,19 +19,55 @@ public class VertexToVertexFold : IFold
 
     public void Apply(Frame frame)
     {
-        if(VerticesAreTheSame() || VerticesLieOnTheSameEdge(frame)) return;
-        
-        var faceToSplit = FaceQueries.FacesContainingVertexIds(frame, new List<Id> { _vertexIdA, _vertexIdB });
-        if (faceToSplit == null) return;
+        if (VerticesAreTheSame() || VerticesLieOnTheSameEdge(frame)) return;
 
-        FaceCommands.SplitFace(frame, faceToSplit, _vertexIdA, _vertexIdB);
+        var crossedEdges = EdgeQueries.EdgesCrossingEdge(frame, _vertexIdA, _vertexIdB);
+        var sortedVertexIds = crossedEdges.Any()
+            ? GenerateCrossVertices(frame, crossedEdges)
+            : new List<Id> { _vertexIdA, _vertexIdB };
 
-        frame.AddEdge(new Edge
+        var last = sortedVertexIds[0];
+        for (var i = 1; i < sortedVertexIds.Count; i++)
         {
-            Assignment = Assignment.U,
-            FoldAngle = 0,
-            Vertices = new[] { _vertexIdA, _vertexIdB }
-        });
+            var current = sortedVertexIds[i];
+            var faceToSplit = FaceQueries.FacesContainingVertexIds(frame, new List<Id> { last, current });
+
+            FaceCommands.SplitFace(frame, faceToSplit, last, current);
+
+            frame.AddEdge(new Edge
+            {
+                Assignment = Assignment.U,
+                FoldAngle = 0,
+                Vertices = new[] { last, current }
+            });
+            
+            last = current;
+        }
+    }
+
+    private List<Id> GenerateCrossVertices(Frame frame, List<Edge> crossedEdges)
+    {
+        var crossedEdgesWithVerticesIds = new List<(Edge, Id)>();
+        for (var i = 0; i < crossedEdges.Count; i++)
+        {
+            var crossedEdge = crossedEdges[i];
+            var intersectionPoint = EdgeQueries.EdgeToEdgeIntersectionPoint(
+                frame.Vertices[_vertexIdA],
+                frame.Vertices[_vertexIdB],
+                frame.Vertices[crossedEdge.Vertices[0]],
+                frame.Vertices[crossedEdge.Vertices[1]]);
+
+            var intersectionVertex = new Vertex
+                { Coord = new Vector3(intersectionPoint.X, 0, intersectionPoint.Y) };
+            var intersectionVertexId = EdgeCommands.AddVertexToEdge(frame, intersectionVertex, crossedEdge);
+            crossedEdgesWithVerticesIds.Add((crossedEdge, intersectionVertexId));
+        }
+
+        return crossedEdgesWithVerticesIds.Select(tuple => tuple.Item2).OrderBy(v =>
+                frame.Vertices[v].Coord.DistanceSquaredTo(frame.Vertices[_vertexIdA].Coord))
+            .Prepend(_vertexIdA)
+            .Append(_vertexIdB)
+            .ToList();
     }
 
     private bool VerticesLieOnTheSameEdge(Frame frame)
