9d606ff4c9b7f1b167b0969feb689b1990fae349
Emil Bohleber <me@limered.de>
2025-03-13 22:26:27 +0100

    refactor: simplify EdgeStrip by removing unused properties and methods

--- valleyfold/src/Folding/EdgeStrip.cs
+++ valleyfold/src/Folding/EdgeStrip.cs
@@ -7,13 +7,9 @@ namespace valleyfold.Folding;
 public class EdgeStrip
 {
     public readonly List<Edge> Edges = new();
-    public readonly List<Edge> EndEdges = new();
-    public readonly List<Vector2> EndPoints = new();
     public readonly List<Vector2> Points = new();
-    public readonly List<Edge> StartEdges = new();
-    public readonly List<Vector2> StartPoints = new();
     public static EdgeStrip Empty => new();
-    public int Length => StartPoints.Count;
+    public int Length => Points.Count;
 
     public void AddSingle(Vector2 point, Edge edge)
     {
@@ -25,43 +21,5 @@ public class EdgeStrip
     {
         Points.Insert(0, point);
         Edges.Insert(0, edge);
-    }
-
-    public void Add((Vector2 pointA, Edge edgeA, Vector2 pointB, Edge edgeB) points)
-    {
-        StartPoints.Add(points.pointA);
-        EndPoints.Add(points.pointB);
-        StartEdges.Add(points.edgeA);
-        EndEdges.Add(points.edgeB);
-    }
-
-    public void Prepend((Vector2 pointA, Edge edgeA, Vector2 pointB, Edge edgeB) points)
-    {
-        StartPoints.Insert(0, points.pointA);
-        EndPoints.Insert(0, points.pointB);
-        StartEdges.Insert(0, points.edgeA);
-        EndEdges.Insert(0, points.edgeB);
-    }
-
-    public void Sort()
-    {
-        if (Length <= 1) return; // sorted because its only one element
-
-        var start = StartEdges[0];
-        if (start == EndEdges[1] || start == StartEdges[1])
-        {
-            (StartPoints[0], EndPoints[0]) = (EndPoints[0], StartPoints[0]);
-            (StartEdges[1], EndEdges[1]) = (EndEdges[1], StartEdges[1]);
-        }
-
-        for (var i = 1; i < Length; i++)
-        {
-            var end = EndEdges[i];
-            if (end == EndEdges[i - 1])
-            {
-                (StartPoints[i], EndPoints[i]) = (EndPoints[i], StartPoints[i]);
-                (StartEdges[i], EndEdges[i]) = (EndEdges[i], StartEdges[i]);
-            }
-        }
     }
 }
