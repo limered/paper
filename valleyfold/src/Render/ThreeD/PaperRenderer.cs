68ba517ff563b90c38cb6095befd0f488fb78403
limered <me@limered.de>
2025-03-17 23:43:06 +0100

    feat: simplify folding based on change records

--- valleyfold/src/Render/ThreeD/PaperRenderer.cs
+++ valleyfold/src/Render/ThreeD/PaperRenderer.cs
@@ -26,48 +26,73 @@ public partial class PaperRenderer : Node3D
         {
             _vertices[i] = frame.Vertices[i].Coord.Vector3XZ();
         }
-        
-        // fold vertices on edges
-        var reflectedAroundFaces = new HashSet<Face>();
-        var facesToReflectAround = new Queue<Face>();
-        facesToReflectAround.Enqueue(frame.Faces[0]);
-        while(facesToReflectAround.Any())
+
+        var changes = Statics.ChangeMemory.Changes;
+        foreach (var change in changes)
         {
-            var initialFace = facesToReflectAround.Dequeue();
-            var faceEdges = initialFace.Edges();
-            for (var e = 0; e < faceEdges.Length; e++)
+            for (var v = 0; v < _vertices.Length; v++)
             {
-                var edge = faceEdges[e];
-                if (edge.Assignment == Assignment.B) continue;
-                if (edge.Assignment == Assignment.V)
-                {
-                    // fold every connected vertex around the edge
-                    var v1 = _vertices[edge.Vertices[0].Value];
-                    var v2 = _vertices[edge.Vertices[1].Value];
-                    var foldAngle = edge.FoldAngle;
-                    if (foldAngle == 0) continue;
-                    
-                    // navigate connected faces and collect vertices to rotate
-                    var connectedFace = edge.Faces().First(f => f != initialFace);
-                    if (reflectedAroundFaces.Contains(connectedFace)) continue;
-                    facesToReflectAround.Enqueue(connectedFace);
-                    
-                    var foldAxis = (v2 - v1).Normalized();
-                    var foldAngleInRad = Math.PI * foldAngle;
-                    for(var v = 0; v < connectedFace.Vertices.Count; v++)
-                    {
-                        var vertex = connectedFace.Vertices[v];
-                        var vertexPosition = _vertices[vertex.Value];
-                        var vertexPositionRelativeToEdge = vertexPosition - v1;
-                        var vertexPositionRelativeToEdgeRotated = vertexPositionRelativeToEdge
-                            .Rotated(foldAxis, (float)foldAngleInRad);
-                        _vertices[vertex.Value] = v1 + vertexPositionRelativeToEdgeRotated;
-                    }
-                }
+                var vertex = _vertices[v];
+                var start = _vertices[change.FoldLine.start];
+                var end = _vertices[change.FoldLine.end];
+                
+                // TODO: use vertex id if can be used, else interpolate edge segment
+                if (start.DirectionTo(vertex).Dot(change.StartPoint) < 0) continue;
+                _vertices[v] = ReflectedAroundLine(start, end, vertex);
             }
-            reflectedAroundFaces.Add(initialFace);
         }
         
+        // fold vertices on edges
+        // var reflectedAroundFaces = new HashSet<Face>();
+        // var facesToReflectAround = new Queue<Face>();
+        // facesToReflectAround.Enqueue(frame.Faces[0]);
+        // while(facesToReflectAround.Any())
+        // {
+        //     var initialFace = facesToReflectAround.Dequeue();
+        //     var faceEdges = initialFace.Edges();
+        //     for (var e = 0; e < faceEdges.Length; e++)
+        //     {
+        //         var edge = faceEdges[e];
+        //         if (edge.Assignment == Assignment.B) continue;
+        //         if (edge.Assignment == Assignment.V)
+        //         {
+        //             // fold every connected vertex around the edge
+        //             var v1 = _vertices[edge.Vertices[0].Value];
+        //             var v2 = _vertices[edge.Vertices[1].Value];
+        //             var foldAngle = edge.FoldAngle;
+        //             if (foldAngle == 0) continue;
+        //             
+        //             // navigate connected faces and collect vertices to rotate
+        //             var connectedFace = edge.Faces().First(f => f != initialFace);
+        //             if (reflectedAroundFaces.Contains(connectedFace)) continue;
+        //             facesToReflectAround.Enqueue(connectedFace);
+        //             
+        //             var foldAxis = (v2 - v1).Normalized();
+        //             var foldAngleInRad = Math.PI * foldAngle;
+        //             for(var v = 0; v < connectedFace.Vertices.Count; v++)
+        //             {
+        //                 var vertex = connectedFace.Vertices[v];
+        //                 var vertexPosition = _vertices[vertex.Value];
+        //                 var vertexPositionRelativeToEdge = vertexPosition - v1;
+        //                 var vertexPositionRelativeToEdgeRotated = vertexPositionRelativeToEdge
+        //                     .Rotated(foldAxis, (float)foldAngleInRad);
+        //                 _vertices[vertex.Value] = v1 + vertexPositionRelativeToEdgeRotated;
+        //             }
+        //         }
+        //     }
+        //     reflectedAroundFaces.Add(initialFace);
+        // }
+        
         FaceRendering.RenderFaces(frame.Faces, _vertices.ToList());
+    }
+
+    private Vector3 ReflectedAroundLine(Vector3 foldLineStart, Vector3 foldLineEnd, Vector3 vertex)
+    {
+        var foldAxis = (foldLineEnd - foldLineStart).Normalized();
+        var foldAngle = Math.PI * 1f;
+        var vertexPositionRelativeToEdge = vertex - foldLineStart;
+        var vertexPositionRelativeToEdgeRotated = vertexPositionRelativeToEdge
+            .Rotated(foldAxis, (float)foldAngle);
+        return foldLineStart + vertexPositionRelativeToEdgeRotated;
     }
 }
