11cb0c1108fbe5e7e5b6326043b3ad1b5da89e3d
limered <me@limered.de>
2025-03-17 23:41:10 +0100

    feat: enhance ChangeMemory to provide read-only access to changes

--- valleyfold/src/ChangeTracking/ChangeMemory.cs
+++ valleyfold/src/ChangeTracking/ChangeMemory.cs
@@ -1,10 +1,16 @@
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace valleyfold.ChangeTracking;
 
 public class ChangeMemory
 {
-    private List<ChangeRecord> _changes = new();
-    
-    
+    private readonly List<ChangeRecord> _changes = new();
+
+    public ReadOnlyCollection<ChangeRecord> Changes => new(_changes);
+
+    public void AddChange(ChangeRecord changeRecord)
+    {
+        _changes.Add(changeRecord);
+    }
 }
